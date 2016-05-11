5c2236d45c2c8a881a84f3bcd2644c4462a8d551
Fumiya Amano <fumiya.amano@gree.co.jp>
2016-05-11 18:25:44 +0900

    add moving back image

---
+++ mockone/Assets/BackGroundScroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundScroll : MonoBehaviour {
+
+	public float speed = 0.01f;			// スクロールするスピード
+	private GameObject camera;
+	private Vector3 offset;
+
+	void Start() {
+		camera = GameObject.FindGameObjectWithTag ("MainCamera");
+		offset = camera.transform.position;
+	}
+	// Update is called once per frame
+	void Update () {
+		offset = camera.transform.position;
+		offset.Scale (new Vector3 (1.0f / transform.localScale.x, 1.0f / transform.localScale.y, 1.0f / transform.localScale.z));
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);	// マテリアルにオフセットを設定する.
+		transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y, 1);
+	}
+}
--- mockone/Assets/Sample/guruguru/Player.cs
+++ mockone/Assets/Sample/guruguru/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	private float first_speed = 10.0f;
+	private float firstSpeed = 10.0f;
 	private Rigidbody playerRigidbody;
 	private GameObject playerCamera;
 	public GameObject star;
@@ -15,30 +15,30 @@ public class Player : MonoBehaviour {
 	public bool furiko = false;
 	private float gravity = 10f;
 
+	private float maxSpeed = 100.0f;
+
 	// Use this for initialization
 	void Start () {
 		playerRigidbody = this.GetComponent<Rigidbody> ();
-		playerRigidbody.velocity = new Vector3 (0, first_speed, 0);
+		playerRigidbody.velocity = new Vector3 (0, firstSpeed, 0);
 		playerCamera = this.GetComponentInChildren<Camera> ().gameObject;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		//押した瞬間
 		if (Input.GetMouseButtonDown(0)) {
-			var mouseScreenPos = Input.mousePosition;
-			mouseScreenPos.z = 30.0f;
-			var mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
-			activeStar = GameObject.Instantiate (star, mouseWorldPos, star.transform.localRotation) as GameObject;
-
-			playerCamera.transform.parent = null;
-			power = playerRigidbody.velocity.sqrMagnitude / ((transform.position - activeStar.transform.position).magnitude * playerRigidbody.mass);
+			CreateStar ();
 		}
 
 		if (Input.GetMouseButtonUp (0)) {
 			playerCamera.transform.parent = this.transform;
+			activeStar = null;
 		}
 
+		//押している間
 		if (Input.GetMouseButton (0)) {
+			CreateStar ();
 			if (furiko) {
 				Vector3 nowPower = (activeStar.transform.position - transform.position).normalized * power
 				                   + Vector3.down * gravity / playerRigidbody.mass;
@@ -47,6 +47,7 @@ public class Player : MonoBehaviour {
 				playerRigidbody.AddForce ((activeStar.transform.position - transform.position).normalized * power);
 			}
 		} else {
+			playerCamera.transform.parent = this.transform;
 			var cameraLocalPosition = playerCamera.transform.localPosition;
 			var cameraLocalPositionZ = cameraLocalPosition.z;
 			cameraLocalPosition.z = 0;
@@ -61,6 +62,22 @@ public class Player : MonoBehaviour {
 			playerCamera.transform.localPosition = cameraLocalPosition;
 		}
 			
+		//速度補正
+		if (playerRigidbody.velocity.sqrMagnitude > maxSpeed * maxSpeed) {
+			Debug.Log ("Speed Over!");
+			playerRigidbody.velocity = playerRigidbody.velocity.normalized * maxSpeed;
+		}
+	}
 
+	void CreateStar() {
+		if (!activeStar) {
+			var mouseScreenPos = Input.mousePosition;
+			mouseScreenPos.z = 30.0f;
+			var mouseWorldPos = Camera.main.ScreenToWorldPoint (mouseScreenPos);
+			activeStar = GameObject.Instantiate (star, mouseWorldPos, star.transform.localRotation) as GameObject;
+
+			playerCamera.transform.parent = null;
+			power = playerRigidbody.velocity.sqrMagnitude / ((transform.position - activeStar.transform.position).magnitude * playerRigidbody.mass);
+		}
 	}
 }
